8272abffb733bca3553134bbd0a0b84fc54051cd
Prestoj01 <prestoj@clemson.edu>
2024-09-26 20:15:10 -0400

    Project Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 function App() {
+    const [typing, setTyping] = useState(false);
     const [messages, setMessages] = useState([
         {
-            message: "Hello, I am ChatGPT!",
+            message: "Hello, I am Presto, your personal Fitness Pal!",
             sender: "ChatGPT",
             direction: "incoming"
         }
@@ -20,9 +23,62 @@ function App() {
             direction: "outgoing"
         }
 
-      const newMessages = [...messages, newMessage];
+        const newMessages = [...messages, newMessage];
+        
+        // update our messages state
+        setMessages(newMessages);
 
-      setMessages(newMessages);
+        // set a typing indicator
+        setTyping(true);
+
+        //process message to GPT
+        await processMessageToChatGPT(newMessages);
+    }
+
+    async function processMessageToChatGPT(chatMessages) {
+        let apiMessages = chatMessages.map((messageObject) => {
+            let role = "";
+            if(messageObject.sender === "ChatGPT") {
+                role="assistant"
+            } else {
+                role = "user"
+            }
+            return { role: role, content: messageObject.message }
+        })
+
+        const systemMessage = {
+            role: "system",
+            content: "You are a fitness coach, give advice on losing weight and gaining muscle by figuring out the best regiment for each person based on weight and height."
+        }
+
+        const apiRequestBody = {
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                systemMessage,
+                ...apiMessages
+            ]
+        }
+
+        await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + API_KEY,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(apiRequestBody)
+        }).then((data) => {
+            return data.json();
+        }).then((data) => {
+            console.log(data);
+            console.log(data.choices[0].message.content);
+            setMessages(
+                [...chatMessages, {
+                    message: data.choices[0].message.content,
+                    sender: "ChatGPT"
+                }]
+            );
+            setTyping(false);
+        });
     }
 
     return (
@@ -30,7 +86,10 @@ function App() {
             <div style={{ position: "relative", height: "800px", width: "700px"}}>
                 <MainContainer>
                     <ChatContainer>
-                        <MessageList>
+                        <MessageList
+                            scrollBehavior='smooth'
+                            typingIndicator={typing ? <TypingIndicator content="Presto is typing" /> : null}
+                        >
                             {messages.map((message, i) => {
                                 return <Message key={i} model={message} />
                             })}
